731c835baa2b91384aee58b6f8b39abed2d9f308
Jing Li <thyrlian@gmail.com>
2020-02-02 04:00:51 +0100

    Add script to generate html from many template components

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TechNewsletter
+
+## Setup
+
+* Run `bundle install` to install all necessary dependencies
---
+++ converter.rb
@@ -0,0 +1,25 @@
+#! /usr/bin/env ruby
+
+require 'nokogiri'
+
+def parse_html(filename)
+  directory = 'template/'
+  return File.open(directory + filename) { |f| Nokogiri::HTML(f) }
+end
+
+def parse_fragment(filename)
+  directory = 'template/'
+  return Nokogiri::HTML::DocumentFragment.parse(File.read(directory + filename))
+end
+
+doc = parse_html('base.html')
+fragment_masthead = parse_fragment('masthead.html')
+fragment_content = parse_fragment('content.html')
+fragment_article = parse_fragment('article.html')
+fragment_imprint = parse_fragment('imprint.html')
+
+node_content = doc.at('body').add_child(fragment_masthead).first.add_next_sibling(fragment_content).first
+node_content.add_next_sibling(fragment_imprint)
+node_content.add_child(fragment_article)
+
+File.write('newsletter.html', doc.to_html)
