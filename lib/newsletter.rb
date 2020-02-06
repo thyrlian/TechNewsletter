e0007f2a7d53ceef1b347e0ed8c09d4f8d5a8b98
Jing Li <thyrlian@gmail.com>
2020-02-06 23:04:08 +0100

    Add Parser

---
+++ lib/newsletter.rb
@@ -0,0 +1,1 @@
+Dir.glob(File.expand_path(File.dirname(__FILE__)) + '/newsletter/**/*.rb').each { |file| require file }
---
+++ lib/newsletter/parser.rb
@@ -0,0 +1,40 @@
+module Newsletter
+  class Parser
+    INDENT_SIZE = 2
+    TAG_BEGINNING_DELIMITER = '⇥'
+    TAG_ENDING_DELIMITER = '⇤'
+
+    class << self
+      def read(file)
+        if (!File.exist?(file))
+          fail(RuntimeError, "#{file} doesn't exist.")
+        end
+        File.readlines(file).each do |line|
+          line = normalize(line)
+        end
+      end
+
+      def get_tag(line)
+        regex = /^\s*?#{TAG_BEGINNING_DELIMITER}(.+)#{TAG_ENDING_DELIMITER}/
+        match_data = regex.match(line)
+        return match_data ? match_data[1] : nil
+      end
+
+      def get_indent(line)
+        regex = /^(\s+)#{TAG_BEGINNING_DELIMITER}.+#{TAG_ENDING_DELIMITER}/
+        match_data = regex.match(line)
+        return match_data ? match_data[1].length / INDENT_SIZE : 0
+      end
+
+      def normalize(line)
+        regex = /^\t+/
+        match_data = regex.match(line)
+        if match_data
+          return line.gsub(regex, "\s" * INDENT_SIZE * match_data[0].split('').length)
+        else
+          return line
+        end
+      end
+    end
+  end
+end
