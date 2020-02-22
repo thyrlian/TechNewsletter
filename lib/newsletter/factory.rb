a8d902fa0cc90c88108e94decd7b0424f6307be0
Jing Li <thyrlian@gmail.com>
2020-02-23 00:53:52 +0100

    Improve author rendering

--- lib/newsletter/factory.rb
+++ lib/newsletter/factory.rb
@@ -63,15 +63,35 @@ module Newsletter
     def p_article(node)
       title = node[:children]['Title'][:data]
       author = node[:children]['Author'][:data]
-      author_avatar = node[:children]['Author'][:children]['AuthorAvatar'][:data]
+      author_tree = node[:children]['Author'][:children]
+      author_avatar = nil
+      author_link = nil
+      if author_tree.keys.include?('Avatar')
+        author_avatar = author_tree['Avatar'][:data]
+      elsif author_tree.keys.include?('Gravatar')
+        author_avatar = Gravatar.get_image_url(author_tree['Gravatar'][:data], 200)
+      end
+      if author_tree.keys.include?('Link')
+        author_link = author_tree['Link'][:data]
+      end
       outline = node[:children]['Outline'][:data]
       read_more = node[:children]['ReadMore'][:data]
       fragment_article = parse_fragment('article.html')
       fragment_article.css('.title > span').first.content = title
-      fragment_article.css('.author > img').first['src'] = author_avatar
       fragment_article.css('.author > span').first.content = author
       fragment_article.css('.outline > p').first.content = outline
       fragment_article.css('.cta-read-more-button > a').first['href'] = read_more
+      if author_avatar.nil?
+        fragment_article.css('.author > img').remove
+      else
+        fragment_article.css('.author > img').first['src'] = author_avatar
+      end
+      unless author_link.nil?
+        author_element = fragment_article.css('.author > span').first
+        author_element.replace('<a>' + author_element.to_xml + '</a>')
+        fragment_article.css('.author > a').first['href'] = author_link
+        fragment_article.css('.author > a').first['style'] = 'text-decoration: none;'
+      end
       @doc.at('#content').add_child(fragment_article)
     end
 
