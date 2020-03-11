aa5fb79c1a680b6e431f69d7f755a01882e8832a
Jing Li <thyrlian@gmail.com>
2020-03-12 00:23:48 +0100

    Change FontAwesome to local SVG

--- lib/newsletter/printers/socialmedia_printer.rb
+++ lib/newsletter/printers/socialmedia_printer.rb
@@ -2,8 +2,6 @@ module Newsletter
   class SocialMediaPrinter < Printer
     class << self
       def print(doc, node)
-        fragment_social_media_script = MLParserWrapper.parse_fragment('social-media-script.html')
-        doc.at('head').add_child(fragment_social_media_script)
         fragment_social_media = MLParserWrapper.parse_fragment('social-media.html')
         social_media_section = fragment_social_media.css('#social-media').first
         node[:children].each do |media, username|
