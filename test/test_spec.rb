f29f777e1b1f0c820156b7efa6ad0ccc788dee0e
Jing Li <thyrlian@gmail.com>
2020-05-23 00:03:24 +0200

    Add test for Spec

--- test/test_spec.rb
+++ test/test_spec.rb
@@ -1,4 +1,11 @@
 require_relative 'helper'
 
 class TestSpec < Minitest::Test
+  def setup
+    @spec = Spec.new
+  end
+
+  def test_constructor
+    assert_equal({}, @spec.instance_variable_get(:@tree))
+  end
 end
